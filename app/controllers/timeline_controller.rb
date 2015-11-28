09cf2c8b9b11eb26a5abd2d5c4868e20845e7708
bradkim <bradkim@joongang.co.kr>
2015-11-28 01:44:52 +0000

    first commit

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,15 +1,18 @@
 class TimelineController < ApplicationController
+    before_action :authenticate_user!, except: :index
+    
     def index
         @blogs = Blog.all.reverse
+        #@My_blogs = Blog.all.where(user_id: current_user.id)
     end    
     
     def write
-        Blog.create(content: params[:naeyong])
+        Blog.create(user_id: current_user.id, content: params[:naeyong])
         redirect_to :root
     end
     
     def comment
-        Comment.create(blog_id: params[:blog_id], msg: params[:comment])
+        Comment.create(user_id: current_user.id, blog_id: params[:blog_id], msg: params[:comment])
         redirect_to :root
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'timeline#index'
   post '/write' => 'timeline#write'
   post '/comment' => 'timeline#comment'
